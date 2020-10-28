946b412cfce87e6194a4cec015d3aed62c22f1ab
Aluno <caiquegressoni@gmail.com>
2020-10-28 12:15:53 -0200

    Navegação

--- ionic/curso-spring-ionic-frontend/src/pages/home/home.ts
+++ ionic/curso-spring-ionic-frontend/src/pages/home/home.ts
@@ -1,15 +1,36 @@
+  
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
+
+
 @IonicPage()
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  creds: CredenciaisDTO =  {
+    email: "",
+    senha: ""
+  };
+
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+
+  loging(){
+    console.log(this.creds);
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 }
